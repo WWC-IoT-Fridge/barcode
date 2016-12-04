c01b498ac6efb243cc1bd5200a998b2cf5c0e72a
also doesn't matter <doesn't matter>
2016-12-04 12:16:12 -0800

    Maya: button doing stuff

--- onboard/main.js
+++ onboard/main.js
@@ -16,22 +16,23 @@
 
 
 var APP_NAME = "IoT Digital Write" ;
-var cfg = require("./cfg-app-platform.js")() ;          // init and config I/O resources
-var test= require("./cfg-app-platform.js")();
+var mraa = require("mraa");
+var blue = require("./cfg-app-platform.js")() ;          // init and config I/O resources
+var red = require("./cfg-app-platform.js")();
 console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n") ;   // poor man's clear console
 console.log("Initializing " + APP_NAME) ;
 
 // confirm that we have a version of libmraa and Node.js that works
 // exit this app if we do not
 
-cfg.identify() ;                // prints some interesting platform details to console
+blue.identify() ;                // prints some interesting platform details to console
 
-if( !cfg.test() ) {
+if( !blue.test() ) {
     process.exitCode = 1 ;
     throw new Error("Call to cfg.test() failed, check console messages for details.") ;
 }
 
-if( !cfg.init() ) {
+if( !blue.init() ) {
     process.exitCode = 1 ;
     throw new Error("Call to cfg.init() failed, check console messages for details.") ;
 }
@@ -42,16 +43,16 @@ var redList = ["Thai curry", "Ketchup", "Carrots"];
 var Color = function(r, g, b) {
     this.red = r;
     this.green = g;
-    this.blue = b
-}
+    this.blue = b;
+};
 
 var blueColor = new Color(0, 0x3E, 0x62);
 var redColor = new Color(0x62, 0, 0);
 
+
+
 var writeWithPauses = function(list, pauseTime) {
     return new Promise( function (resolve, reject) {
-        var iter = iterateList(list);
-        var intID = setInterval(iter.next, pauseTime);
         var iterateList = function (list) {
             var i = 0;
             return {
@@ -66,14 +67,14 @@ var writeWithPauses = function(list, pauseTime) {
                 }
             };
         };
+        var iter = iterateList(list);
+        var intID = setInterval(iter.next, pauseTime);
     });
 };
 var clearAndWrite = function(str) {
     display.clear();
     display.home();
     display.write(str);
-    //THIS MIGHT WORK
-    display.scroll(true);
 };
 var showList = function (color, list) {
     display.setColor(color.red, color.green, color.blue);
@@ -86,45 +87,27 @@ var showList = function (color, list) {
 
 // configure (initialize) our I/O pins for usage (gives us an I/O object)
 // configuration is based on parameters provided by the call to cfg.init()
+
 var lcd = require('jsupm_i2clcd');
 var display = new lcd.Jhd1313m1(0, 0x3E, 0x62);
-showList(blueColor, blueList).then(function() {showList(redColor, redList)});
-test.io = new test.mraa.Gpio(6, true, false);
-test.io = new test.mraa.Gpio(6, true, false);
-test.io.dir(test.mraa.DIR_OUT);
-cfg.io = new cfg.mraa.Gpio(cfg.ioPin, cfg.ioOwner, cfg.ioRaw) ;
-cfg.io.dir(cfg.mraa.DIR_OUT) ;                  // configure the gpio pin as an output
+
+red.io = new red.mraa.Gpio(6, true, false);
+red.io = new red.mraa.Gpio(6, true, false);
+red.io.dir(red.mraa.DIR_OUT);
+blue.io = new blue.mraa.Gpio(blue.ioPin, blue.ioOwner, blue.ioRaw) ;
+blue.io.dir(blue.mraa.DIR_OUT) ;                  // configure the gpio pin as an output
 
 var button = new mraa.Gpio(7);
-var buttonPressed = function () {console.log.("button!\n");}
-button.isr(mraa.EDGE_RISING, buttonPressed);
-
-//var intervalID = setInterval(periodicActivity, 1000) ;  // start the periodic write
-cfg.io.write(0);
-test.io.write(0);
-var testActivity = function () {
-    cfg.io.write(0);
-    test.io.write(1);
-    process.stdout.write('wrote 0 to cfg, 1 to test');
-}
-
-var secondTest = function () {
-    cfg.io.write(1);
-    test.io.write(0);
-    process.stdout.write("okay we're done now, switched it");
-}
-
 var buttonPressed = function () {
-    fakeBlueRequest
-}
+    console.log("pressed");
+    showList(blueColor, blueList).then(function() {showList(redColor, redList)});
+};
 
-
-
-
-// type process.exit(0) in debug console to see
-// the following message be emitted to the debug console
-
-process.on("exit", function(code) {
-    //clearInterval(intervalID) ;
-    console.log("\nExiting " + APP_NAME + ", with code:", code) ;
-}) ;
+console.log("before button\n");
+button.isr(mraa.EDGE_BOTH, buttonPressed);
+console.log("After button\n");
+blue.io.write(0);
+red.io.write(1);
+(function wait(){
+ setTimeout(wait, 1000);
+ })();
